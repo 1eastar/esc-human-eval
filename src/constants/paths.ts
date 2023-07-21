c164ae2335f4dda8cc255f8a739e8c687564af0b
1eastar <ehdwls6703@gmail.com>
2023-07-21 18:01:48 +0900

    fix

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 export const paths = {
-  SCORES: path.join(process.cwd(), "/results/scores.json"),
+  SCORES: path.join(process.cwd(), "/scores.json"),
   FiveShot: path.join(process.cwd(), "/results/5shot_result.json"),
   USR: path.join(process.cwd(), "/results/usr_result.json"),
   BOTH: path.join(process.cwd(), "/results/both_result.json"),
