6f52495afcd34a1bd2565905bb744d123eeb234f
1eastar <ehdwls6703@gmail.com>
2023-07-21 11:28:22 +0900

    revert api

--- pages/api/score.ts
+++ pages/api/score.ts
@@ -28,43 +28,40 @@ export default async function handler(req, res) {
     const { convId, turnId, qKey, tab, score }: ResponseBody = req.body
 
     const currentScoreData = scoreData.find(s => s.conv_id === convId && s.turn_id === turnId)
-    try {
-      if (currentScoreData) {
-        const newScores = scoreData.map((_score: ScoreData) => {
-          if (_score.conv_id === convId && _score.turn_id === turnId) {
-            const questionScores = _score?.scores[qKey]
-            const newQuestionScores = makeScoreScheme(score, tab, questionScores)
-            _score.scores[qKey] = newQuestionScores
-          }
   
-          return _score
-        })
-  
-        const updatedScores = JSON.stringify(newScores)
-        await fsPromises.writeFile(paths.SCORES, updatedScores)
-        res.status(200).json(newScores)
-  
-      } else {
-        const newScoreData: ScoreData = {
-          conv_id: convId,
-          turn_id: turnId,
-          scores: {
-            [qKey]: {
-              [Tab.USR]: -1,
-              [Tab.BOTH]: -1,
-              [Tab.GOLD]: -1,
-              [Tab.FIVESHOT]: -1,
-              [tab]: score
-            }
+    if (currentScoreData) {
+      const newScores = scoreData.map((_score: ScoreData) => {
+        if (_score.conv_id === convId && _score.turn_id === turnId) {
+          const questionScores = _score?.scores[qKey]
+          const newQuestionScores = makeScoreScheme(score, tab, questionScores)
+          _score.scores[qKey] = newQuestionScores
+        }
+
+        return _score
+      })
+
+      const updatedScores = JSON.stringify(newScores)
+      await fsPromises.writeFile(paths.SCORES, updatedScores)
+      res.status(200).json(newScores)
+
+    } else {
+      const newScoreData: ScoreData = {
+        conv_id: convId,
+        turn_id: turnId,
+        scores: {
+          [qKey]: {
+            [Tab.USR]: -1,
+            [Tab.BOTH]: -1,
+            [Tab.GOLD]: -1,
+            [Tab.FIVESHOT]: -1,
+            [tab]: score
           }
         }
-        scoreData.push(newScoreData)
-        const updatedScores = JSON.stringify(scoreData)
-        await fsPromises.writeFile(paths.SCORES, updatedScores)
-        res.status(200).json(scoreData)
       }
-    } catch (e) {
-      res.status(400)
+      scoreData.push(newScoreData)
+      const updatedScores = JSON.stringify(scoreData)
+      await fsPromises.writeFile(paths.SCORES, updatedScores)
+      res.status(200).json(scoreData)
     }
   }
 }
