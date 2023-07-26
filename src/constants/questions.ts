bc347ddd3c576eaafa86b515792ad6c2cc0c6482
1eastar <ehdwls6703@gmail.com>
2023-07-26 14:27:03 +0900

    add question

--- src/constants/questions.ts
+++ src/constants/questions.ts
@@ -21,4 +21,32 @@ export const Questions: Question[] = [
     key: "3",
     content: "Is the response grounded to inferred ToM?",
   },
+  {
+    key: "4",
+    content: "Fluency: Which model’s response is more fluent?",
+  },
+  {
+    key: "5",
+    content: "Consistency: Which model’s response is more consistent with its role? Which model’s response is less self-contradictory?",
+  },
+  {
+    key: "6",
+    content: "Coherence: Which model’s response is more on-topic and in-depth? Which model’s response is more natural in topic transition?",
+  },
+  {
+    key: "7",
+    content: "Identification: Which model’s response is more skillful in identifying the user’s problem?",
+  },
+  {
+    key: "8",
+    content: "Comforting: Which model’s response is better at comforting the user?",
+  },
+  {
+    key: "9",
+    content: "Suggestion: Which model can give more helpful and informative suggestions?",
+  },
+  {
+    key: "10",
+    content: "Overall: Which model’s response is generally better?",
+  },
 ]
